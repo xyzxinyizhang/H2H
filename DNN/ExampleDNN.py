dbbccd246f94d848bdc030aa04bf43a881be8f40
xinyi zhang <31314652+xyzxinyizhang@users.noreply.github.com>
2022-04-28 21:51:31 -0400

    Add files via upload

---
+++ DNN/ExampleDNN.py
@@ -0,0 +1,67 @@
+M1L1 = {'LayerType': 'CNN', 'LayerName': 'M1L1', 'M': 64,  'N': 3, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M1L2 = {'LayerType': 'CNN', 'LayerName': 'M1L2', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M1L3 = {'LayerType': 'CNN', 'LayerName': 'M1L3', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M1L4 = {'LayerType': 'CNN', 'LayerName': 'M1L4', 'M': 64,  'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M1L5 = {'LayerType': 'CNN', 'LayerName': 'M1L5', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M1L6 = {'LayerType': 'CNN', 'LayerName': 'M1L6', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M1L7 = {'LayerType': 'CNN', 'LayerName': 'M1L7', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M1L8 = {'LayerType': 'CNN', 'LayerName': 'M1L8', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M1L9 = {'LayerType': 'CNN', 'LayerName': 'M1L9', 'M': 128, 'N': 64, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M1L10 = {'LayerType': 'CNN', 'LayerName': 'M1L10', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M1L11 = {'LayerType': 'CNN', 'LayerName': 'M1L11', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M1L12 = {'LayerType': 'CNN', 'LayerName': 'M1L12', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M1L13 = {'LayerType': 'CNN', 'LayerName': 'M1L13', 'M': 256, 'N': 384, 'R': 14, 'C': 14, 'K': 3, 'S': 1, 'P': 1}
+M1L14 = {'LayerType': 'CNN', 'LayerName': 'M1L14', 'M': 256, 'N': 256, 'R': 14, 'C': 14, 'K': 3, 'S': 1, 'P': 1}
+M1L15 = {'LayerType': 'CNN', 'LayerName': 'M1L15', 'M': 256, 'N': 256, 'R': 14, 'C': 14, 'K': 3, 'S': 1, 'P': 1}
+M1L16 = {'LayerType': 'CNN', 'LayerName': 'M1L16', 'M': 256, 'N': 256, 'R': 14, 'C': 14, 'K': 3, 'S': 1, 'P': 1}
+M1L17 = {'LayerType': 'CNN', 'LayerName': 'M1L17', 'M': 512, 'N': 256, 'R': 7, 'C': 7, 'K': 3, 'S': 1, 'P': 1}
+M1L18 = {'LayerType': 'CNN', 'LayerName': 'M1L18', 'M': 512, 'N': 512, 'R': 7, 'C': 7, 'K': 3, 'S': 1, 'P': 1}
+M1L19 = {'LayerType': 'CNN', 'LayerName': 'M1L19', 'M': 512, 'N': 512, 'R': 7, 'C': 7, 'K': 3, 'S': 1, 'P': 1}
+M1L20 = {'LayerType': 'CNN', 'LayerName': 'M1L20', 'M': 512, 'N': 512, 'R': 7, 'C': 7, 'K': 3, 'S': 1, 'P': 1}
+
+M1 = {'M1L1': M1L1, 'M1L2': M1L2, 'M1L3': M1L3,      'M1L4': M1L4, 'M1L5': M1L5,
+      'M1L6': M1L6, 'M1L7': M1L7, 'M1L8': M1L8,       'M1L9': M1L9, 'M1L10': M1L10,
+      'M1L11': M1L11, 'M1L12': M1L12, 'M1L3': M1L13, 'M1L14': M1L14, 'M1L5': M1L15,
+      'M1L16': M1L16, 'M1L17': M1L17, 'M1L18': M1L18, 'M1L19': M1L19, 'M1L20': M1L20
+      }
+
+M2L1 = {'LayerType': 'CNN', 'LayerName': 'M2L1', 'M': 64,  'N': 3, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M2L2 = {'LayerType': 'CNN', 'LayerName': 'M2L2', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M2L3 = {'LayerType': 'CNN', 'LayerName': 'M2L3', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M2L4 = {'LayerType': 'CNN', 'LayerName': 'M2L4', 'M': 64,  'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M2L5 = {'LayerType': 'CNN', 'LayerName': 'M2L5', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M2L6 = {'LayerType': 'CNN', 'LayerName': 'M2L6', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M2L7 = {'LayerType': 'CNN', 'LayerName': 'M2L7', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M2L8 = {'LayerType': 'CNN', 'LayerName': 'M2L8', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M2L9 = {'LayerType': 'CNN', 'LayerName': 'M2L9', 'M': 128, 'N': 64, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M2L10 = {'LayerType': 'CNN', 'LayerName': 'M2L10', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M2L11 = {'LayerType': 'CNN', 'LayerName': 'M2L11', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M2L12 = {'LayerType': 'CNN', 'LayerName': 'M2L12', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+
+M2 = {'M2L1': M2L1, 'M2L2': M2L2, 'M2L3': M2L3,      'M2L4': M2L4, 'M2L5': M2L5,
+      'M2L6': M2L6, 'M2L7': M2L7, 'M2L8': M2L8,       'M2L9': M2L9, 'M2L10': M2L10,
+      'M2L11': M2L11, 'M2L12': M2L12
+      }
+
+M3L1 = {'LayerType': 'CNN', 'LayerName': 'M3L1', 'M': 64,  'N': 3, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M3L2 = {'LayerType': 'CNN', 'LayerName': 'M3L2', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M3L3 = {'LayerType': 'CNN', 'LayerName': 'M3L3', 'M': 64, 'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M3L4 = {'LayerType': 'CNN', 'LayerName': 'M3L4', 'M': 64,  'N': 64, 'R': 112, 'C': 112, 'K': 3, 'S': 1, 'P': 1}
+M3L5 = {'LayerType': 'CNN', 'LayerName': 'M3L5', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M3L6 = {'LayerType': 'CNN', 'LayerName': 'M3L6', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M3L7 = {'LayerType': 'CNN', 'LayerName': 'M3L7', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M3L8 = {'LayerType': 'CNN', 'LayerName': 'M3L8', 'M': 64, 'N': 64, 'R': 56, 'C': 56, 'K': 3, 'S': 1, 'P': 1}
+M3L9 = {'LayerType': 'CNN', 'LayerName': 'M3L9', 'M': 128, 'N': 64, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M3L10 = {'LayerType': 'CNN', 'LayerName': 'M3L10', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M3L11 = {'LayerType': 'CNN', 'LayerName': 'M3L11', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+M3L12 = {'LayerType': 'CNN', 'LayerName': 'M3L12', 'M': 128, 'N': 128, 'R': 28, 'C': 28, 'K': 3, 'S': 1, 'P': 1}
+
+M3 = {'M3L1': M3L1, 'M3L2': M3L2, 'M3L3': M3L3,      'M3L4': M3L4, 'M3L5': M3L5,
+      'M3L6': M3L6, 'M3L7': M3L7, 'M3L8': M3L8,       'M3L9': M3L9, 'M3L10': M3L10,
+      'M3L11': M3L11, 'M3L12': M3L12
+      }
+
+DNN = [M1, M2, M3]
+
+if __name__ == "__main__":
+    print('this is an example modality')
